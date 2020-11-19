09a6c042e0463a5de66076beb5b0d75bbc8baa06
John Ochigbano <johneliochigbano@yahoo.com>
2020-11-18 17:59:20 -0800

    milestone2

---
+++ Analysis/Scripts/poject.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+def load_and_process(url_or_path_to_csv_file):
+
+    df1 = (pd.read_csv(r'\Users\john\Desktop\course-project-solo_318-1\Analysis\listings(2).csv')
+          .rename(columns={"id": "Property_id"})
+          .rename(columns={"name": "Property_name"})
+          .reset_index(drop=True) )
+
+    df2 = (
+            df1
+            .sort_values("Property_id", ascending=True)
+            .drop(columns=['neighbourhood_group'])
+            .drop(columns=['longitude', 'latitude'])
+            .dropna()
+     
+           )
+
+    return df2
